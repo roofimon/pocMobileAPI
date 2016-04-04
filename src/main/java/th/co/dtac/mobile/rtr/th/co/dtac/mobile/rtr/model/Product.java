8b4a67cc2b118d02e69adbbe402fa569933030c0
roofimon <roofimon@gmail.com>
2016-04-04 15:42:36 +0700

    Add product class

--- src/main/java/th/co/dtac/mobile/rtr/th/co/dtac/mobile/rtr/model/Product.java
+++ src/main/java/th/co/dtac/mobile/rtr/th/co/dtac/mobile/rtr/model/Product.java
@@ -1,7 +1,69 @@
 package th.co.dtac.mobile.rtr.th.co.dtac.mobile.rtr.model;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
 /**
  * Created by roofimon on 4/4/2016 AD.
  */
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String productId;
+    private String description;
+    private String imageUrl;
+    private BigDecimal price;
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
