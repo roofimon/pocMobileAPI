d132213c480fbf11b98eb21517925ab39b7677a6
amanurat <assanai17@gmail.com>
2016-04-06 01:34:48 +0700

    Add Simple log to display rest service on root path

--- src/main/java/th/co/dtac/mobile/rtr/MainController.java
+++ src/main/java/th/co/dtac/mobile/rtr/MainController.java
@@ -1,13 +1,21 @@
 package th.co.dtac.mobile.rtr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class MainController {
 
+
+    private static Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping("/")
     public String index() {
+
+        LOGGER.debug("> Greetings Mobile RTR from Spring Boot on path '/' ");
+
         return "Greetings Mobile RTR from Spring Boot!";
     }
 
