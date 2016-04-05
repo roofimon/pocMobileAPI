4e8120fe115149661fcc7d2f5b8fc0e4650263bb
amanurat <assanai17@gmail.com>
2016-04-06 01:35:40 +0700

    Add Spring profiles to support [dev,prod] environment

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,3 @@
+insert into product ( id, productid, description, price ,version ) values (1, 'A01', 'iPhone', 30000, 0);
+insert into product ( id, productid, description, price ,version ) values (2, 'A02', 'iPad', 15000, 0);
+insert into product ( id, productid, description, price ,version ) values (3, 'A03', 'iMac', 25000, 0);
