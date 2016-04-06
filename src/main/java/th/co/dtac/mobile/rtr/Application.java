8fdb68b7225f4638dc2b4a090a303348f7521ae8
amanurat <assanai17@gmail.com>
2016-04-06 14:51:16 +0700

    Refactor import package

--- src/main/java/th/co/dtac/mobile/rtr/Application.java
+++ src/main/java/th/co/dtac/mobile/rtr/Application.java
@@ -1,27 +1,18 @@
 package th.co.dtac.mobile.rtr;
 
-<<<<<<< HEAD
-import java.math.BigDecimal;
-import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import th.co.dtac.mobile.rtr.repository.ProductRepository;
-import th.co.dtac.mobile.rtr.th.co.dtac.mobile.rtr.model.Product;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
