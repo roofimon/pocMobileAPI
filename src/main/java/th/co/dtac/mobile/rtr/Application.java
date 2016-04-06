0502fb8b4aa807f0a38b4716d8f9ddf722fce998
roofimon <roofimon@gmail.com>
2016-04-06 09:32:28 +0700

    Add command line runner

--- src/main/java/th/co/dtac/mobile/rtr/Application.java
+++ src/main/java/th/co/dtac/mobile/rtr/Application.java
@@ -1,24 +1,30 @@
 package th.co.dtac.mobile.rtr;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import th.co.dtac.mobile.rtr.repository.ProductRepository;
+import th.co.dtac.mobile.rtr.th.co.dtac.mobile.rtr.model.Product;
 
 @SpringBootApplication
 public class Application {
-    
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
+
         System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
+
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
     }
-
 }
---
+++ src/main/java/th/co/dtac/mobile/rtr/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package th.co.dtac.mobile.rtr.repository;
+
+/**
+ * Created by roofimon on 4/4/2016 AD.
+ */
+public interface ProductRepository {
+}
