e44cc773cc086eeead290489c049ce1f692b452d
roofimon <roofimon@gmail.com>
2016-04-06 09:32:54 +0700

    Add product repo

--- src/main/java/th/co/dtac/mobile/rtr/repository/ProductRepository.java
+++ src/main/java/th/co/dtac/mobile/rtr/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package th.co.dtac.mobile.rtr.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import th.co.dtac.mobile.rtr.th.co.dtac.mobile.rtr.model.Product;
+
+import java.util.List;
+
 /**
  * Created by roofimon on 4/4/2016 AD.
  */
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByProductName(String productName);
 }
